1749dd4d92ddb795c3525170392457a3cac62921
nofurtherinformation <dhalpern@uchicago.edu>
2024-03-20 09:22:37 -0500

    tina

---
+++ tina/collections/page.js
@@ -0,0 +1,58 @@
+/**
+ * @type {import('tinacms').Collection}
+ */
+export default {
+  label: "Page Content",
+  name: "page",
+  path: "content/page",
+  format: "mdx",
+  fields: [
+    {
+      name: "body",
+      label: "Main Content",
+      type: "rich-text",
+      isBody: true,
+    },
+    // list of "sections"
+    // with section title
+    // and rich-text body
+    {
+      name: "sections",
+      label: "Sections",
+      type: "object",
+      list: true,
+      ui: {
+        itemProps: (item) => {
+          // Field values are accessed by item?.<Field name>
+          return { label: item?.title };
+        },
+        router: ({ document }) => {
+          if (document._sys.filename === "home") {
+            return `/`;
+          }
+          return undefined;
+        },
+      },
+      fields: [
+        {
+          name: "title",
+          label: "Title",
+          type: "string",
+        },
+        {
+          name: "body",
+          label: "Body",
+          type: "rich-text",
+        },
+      ],
+    },
+  ],
+  ui: {
+    router: ({ document }) => {
+      if (document._sys.filename === "home") {
+        return `/`;
+      }
+      return undefined;
+    },
+  },
+};
