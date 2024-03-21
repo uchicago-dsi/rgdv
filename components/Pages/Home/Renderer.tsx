e761a9ce9d0c37985adcebb68ca2d1857872d231
nofurtherinformation <dhalpern@uchicago.edu>
2024-03-21 14:44:02 -0500

    image test + links

--- components/Pages/Home/Renderer.tsx
+++ components/Pages/Home/Renderer.tsx
@@ -1,18 +1,33 @@
-import { HomeProps } from "./types";
-import { TinaMarkdown } from 'tinacms/dist/rich-text'
+import { HomeProps } from "./types"
+import { TinaMarkdown } from "tinacms/dist/rich-text"
 
 export const Renderer: React.FC<HomeProps> = ({ pageInfo }) => {
   const sections = pageInfo.data.page.sections || []
-  
+
   return (
-    <div>
-      <h1><TinaMarkdown content={pageInfo.data.page.body}/></h1>
+    <article className="prose p-4">
+      <h1>Pages</h1>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/admin/index.html">Admin</a>
+        </li>
+      </ul>
+
+      <h1>
+        <TinaMarkdown content={pageInfo.data.page.body} />
+      </h1>
       {sections.map((section: any, i: number) => (
         <div key={i}>
           <h1>{section.title}</h1>
           <TinaMarkdown content={section.body} />
         </div>
       ))}
-    </div>
+    </article>
   )
 }
