fd48e866bb40ee43e0888844ccf2400e6f8eff09
nofurtherinformation <dhalpern@uchicago.edu>
2024-03-21 14:35:30 -0500

    local mdx read fingers crossed

--- hooks/useMdxContent.ts
+++ hooks/useMdxContent.ts
@@ -1,53 +1,60 @@
 import { compile } from '@mdx-js/mdx'
 import { client } from 'tina/__generated__/client'
 import fs from 'fs'
-import path from 'path'
+import path, { parse } from 'path'
+import matter from 'gray-matter';
+import { parseMDX } from "@tinacms/mdx";
 
 const DEV = process.env.NODE_ENV === 'development'
-const convertMdxToRichText = async (mdxContent: any) => {
-  const mdxSource = await compile(mdxContent, {
-    // You can pass in options for the MDX compiler here
-    // e.g., to enable jsx pragma import or other plugins
-  })
-  console.log('mdxSource', mdxSource)
 
-  // This function takes the compiled MDX and renders it to a JSON structure
-  const renderToJson: any = (node: any) => {
-    if (node.type === 'text') {
-      return node.value
-    }
-
-    if (node.type === 'element') {
-      const children = node.children.flatMap(renderToJson)
-      const props = Object.fromEntries(
-        Object.entries(node.properties).map(([key, value]) => [
-          key,
-          renderToJson(value),
-        ])
-      )
-
-      return {
-        type: node.name,
-        props,
-        children,
-      }
-    }
-
-    return null
-  }
-
-  // Render the compiled MDX to a JSON structure
-  const richTextContent = renderToJson(mdxSource.value)
-
-  return richTextContent
+const parseRich = (mdxContent: string) => {
+  return parseMDX(mdxContent, {
+    name: 'body', // This is the key in your data for the rich text content
+    label: 'Body', // This is the label that will be displayed in the TinaCMS sidebar
+    // @ts-ignore
+    component: 'rich-text', // This tells TinaCMS to use the rich text editor component
+    description: 'Enter the main content of the page here', // Optional: Provides a description for this field in the sidebar
+  }, (f: any) => f)
 }
 
 export const getMdxContent = async (contentType: keyof typeof client.queries, relativePath: string) => {
-  if (DEV || true) {
-    return await client.queries[contentType]({ relativePath })
+  if (DEV) {
+    const r = await client.queries[contentType]({ relativePath })
+    return r
   } else {
     const filepath = path.join(process.cwd(), 'content', contentType, relativePath)
     const mdxContent = fs.readFileSync(filepath, 'utf-8')
-    return await convertMdxToRichText(mdxContent)
+    const frontMatter = matter(mdxContent)
+    const data: any = {}
+    data[contentType] = {}
+    data[contentType].id = `content/${contentType}/${relativePath}`
+    data[contentType].__typename = contentType
+    data[contentType].body = parseRich(frontMatter.content)
+    const fmData = frontMatter.data
+    const dataKeys = Object.keys(fmData)
+    for (let i = 0; i < dataKeys.length; i++) {
+      const key = dataKeys[i]!
+      const value = fmData[key]
+      if (Array.isArray(value)) {
+        data[contentType][key] = []
+        for (let j = 0; j < value.length; j++) {
+          const v = value[j] as any
+          const content = parseRich(v.body)
+          const valueOut = {
+            ...v,
+            body: content
+          }
+          data[contentType][key].push(valueOut)
+        }
+      } else {
+        data[contentType][key] = {
+          ...value,
+          body: parseRich(value.body)
+        }
+      }
+    }
+    return {
+      data
+    }
   }
 }
