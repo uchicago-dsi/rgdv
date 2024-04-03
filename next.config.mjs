f7fb3de2d1dd4cb3553d4a90f0058fd484429a8e
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-03 13:57:13 -0500

    config updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,37 @@
 import withBundleAnalyzer from "@next/bundle-analyzer"
 import withPlugins from "next-compose-plugins"
 import { env } from "./env.mjs"
-
+import NodePolyFillPlugin from "node-polyfill-webpack-plugin"
+import path from "path"
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+const config = withPlugins([[new NodePolyFillPlugin(), withBundleAnalyzer({ enabled: env.ANALYZE })]], {
   reactStrictMode: true,
-  experimental: { instrumentationHook: true },
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback = { fs: false }
+  //   }
+    // const _config = {
+    //   ...config,
+    //   experiments: {
+    //     ...config.experiments,
+    //     asyncWebAssembly: true,
+    //   },
+    //   resolve: {
+    //     ...config.resolve,
+    //     fallback: {
+    //       ...config.resolve.fallback,
+    //       modules: ['node_modules'],
+    //         util: path.resolve("node_modules/util/")
+    //     }
+    //   }
+    // }
+    // return config
+  // },
+  experimental: { 
+    instrumentationHook: true, 
+  },
   rewrites() {
     return [
       { source: "/healthz", destination: "/api/health" },
