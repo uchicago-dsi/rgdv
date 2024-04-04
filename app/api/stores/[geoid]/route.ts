bc9d3fc5a81d67f0e98e08a7e48141f17a79d058
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-04 12:20:11 -0500

    reduce deps

--- app/api/stores/[geoid]/route.ts
+++ app/api/stores/[geoid]/route.ts
@@ -21,34 +21,6 @@ const getIsochrone = async (geoid: string) => {
   }
   return isochorones[state]![geoid]
 }
-  
-// const dataPath = path.join(process.cwd(), "public", "data");
-// const storePath = path.join(dataPath, "___grocery_store_sales.parquet");
-
-// const loadParquet = async (path:string) => {
-  
-//   try {
-//     const t0 = performance.now();
-//     console.log('reading file')
-//     // const r = readFileSync(path);
-//     let reader = await ParquetReader.openFile(path);
-
-//     // create a new cursor
-//     let i =0
-//     let cursor = reader.getCursor();
-//     // read all records from the file and print them
-//     let record = null;
-//     while (record = await cursor.next() && i < 10) {
-//       i ++;
-//       console.log(record);
-//     }
-//     return null
-//   } catch (error) {
-//     console.log("Error reading file")
-//     console.error(error);
-//     return null;
-//   }
-// }
 
 export async function GET(request: Request, reqParams: ReqParams) {
   const geoid = reqParams.params.geoid
