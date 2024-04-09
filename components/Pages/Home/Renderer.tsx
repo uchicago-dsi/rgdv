565309f4f430a85a971501f0bec4ce5a15eaa4b3
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-09 15:48:34 -0500

    home page

--- components/Pages/Home/Renderer.tsx
+++ components/Pages/Home/Renderer.tsx
@@ -1,33 +1,97 @@
+import Image from "next/image"
 import { HomeProps } from "./types"
 import { TinaMarkdown } from "tinacms/dist/rich-text"
 
 export const Renderer: React.FC<HomeProps> = ({ pageInfo }) => {
   const sections = pageInfo.data.page.sections || []
-  
-  return (
-    <article className="prose p-4">
-      <h1>Pages</h1>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/map">Map</a>
-        </li>
-        <li>
-          <a href="/admin/index.html">Admin</a>
-        </li>
-      </ul>
 
-      <h1>
-        <TinaMarkdown content={pageInfo.data.page.body} />
-      </h1>
-      {sections.map((section: any, i: number) => (
-        <div key={i}>
-          <h1>{section.title}</h1>
-          <TinaMarkdown content={section.body} />
+  return (
+    <main className="flex flex-col">
+      <div className="relative h-[100vh] w-full" id="main-hero">
+        <div className="">
+          <Image
+            src="/images/hero.png"
+            alt="Picture of the author"
+            fill
+            style={{
+              objectFit: "cover",
+              objectPosition: "top middle",
+            }}
+          />
         </div>
-      ))}
-    </article>
+
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-[#00152c] to-[#00152c]"></div>
+        <div className="absolute inset-x-0 bottom-0 h-[30vh] bg-[#00152c]"></div>
+        <div className="text-canvas flex h-full flex-col justify-end p-4">
+          <div className="z-20 grid grid-flow-col grid-rows-2">
+            <div>
+              <h1 className="text-9xl font-bold">Feeding Fairness</h1>
+            </div>
+            <div>
+              <h2 className="text-3xl font-bold">
+                Understanding the impacts of structural racism &amp; unfair markets on food access
+              </h2>
+            </div>
+          </div>
+          <div className="z-20 grid grid-flow-col grid-rows-2">
+            <div>
+              This tool enables advocates, analytics, and decision-makers to unravel the landscape of inequitable access
+              to food across the US. Use this tool to find opportunities to improve access, break down potential
+              relationships of factors driving inequity, and utilize our data for further analysis and research.
+            </div>
+            <div>
+              Know where you want to see? Search for a state, county, or address below to see a report on food access,
+              market concentration, and structural racism:
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-canvas border-top-1 h-[50vh] bg-[#00152c] p-4">
+        <hr />
+        {/* 4 div flex layout equal widths */}
+        {/* reports, trends, toolkit, about */}
+        <div className="my-4 flex flex-row justify-between gap-4">
+          <div className="flex flex-col border-2 p-4">
+            <h1>Reports</h1>
+            <p>Find reports on food access, market concentration, and structural racism.</p>
+            <p>
+              <i>coming soon...</i>
+            </p>
+          </div>
+          <div className="flex flex-col border-2 p-4">
+            <h1>Trends</h1>
+            <p>Explore trends in food access, market concentration, and structural racism.</p>
+            <p>
+              <i>coming soon...</i>
+            </p>
+          </div>
+          <div className="flex flex-col border-2 p-4">
+            <h1>Toolkit</h1>
+            <p>Use our toolkit to analyze and visualize data on food access.</p>
+            <p>
+              <i>coming soon...</i>
+            </p>
+          </div>
+          <div className="flex flex-col border-2 p-4">
+            <h1>About</h1>
+            <p>Learn more about the project and our team.</p>
+            <p>
+              <i>coming soon...</i>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <article className="prose p-4">
+        <h1>
+          <TinaMarkdown content={pageInfo.data.page.body} />
+        </h1>
+        {sections.map((section: any, i: number) => (
+          <div key={i}>
+            <h1>{section.title}</h1>
+            <TinaMarkdown content={section.body} />
+          </div>
+        ))}
+      </article> */}
+    </main>
   )
 }
