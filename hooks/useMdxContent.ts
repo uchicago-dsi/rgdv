364daecfde324f174a03b95e68e20abfed8ec69c
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-11 14:45:40 -0500

    debug mdx content

--- hooks/useMdxContent.ts
+++ hooks/useMdxContent.ts
@@ -23,6 +23,10 @@ export const getMdxContent = async <T extends any>(contentType: keyof typeof cli
     return r
   } else {
     const filepath = path.join(process.cwd(), 'content', contentType, relativePath)
+    console.log(filepath)
+    // log dirs in current dir
+    const dirs = fs.readdirSync(path.join(process.cwd()))
+    console.log(dirs)
     const mdxContent = fs.readFileSync(filepath, 'utf-8')
     const frontMatter = matter(mdxContent)
     const data: any = {
