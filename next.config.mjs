80380f86f4fe9ea8eed0b6de48a1d06f0d8df25d
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-11 15:48:13 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,40 +8,6 @@ import path from "path"
  */
 const config = withPlugins([[new NodePolyFillPlugin(), withBundleAnalyzer({ enabled: env.ANALYZE })]], {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals.map((external) => {
-        if (typeof external !== "function") return external
-        return ({ context, request }, callback) => {
-          // Check if the request starts with the path to your assets
-          if (request.startsWith("/content")) {
-            // Do not externalize if the request is for server-assets, thus including it in the bundle
-            return callback()
-          }
-          // Use the external function as default for other requests
-          return external({ context, request }, callback)
-        }
-      })
-    }
-    return config
-  },
-  // const _config = {
-  //   ...config,
-  //   experiments: {
-  //     ...config.experiments,
-  //     asyncWebAssembly: true,
-  //   },
-  //   resolve: {
-  //     ...config.resolve,
-  //     fallback: {
-  //       ...config.resolve.fallback,
-  //       modules: ['node_modules'],
-  //         util: path.resolve("node_modules/util/")
-  //     }
-  //   }
-  // }
-  // return config
-  // },
   experimental: {
     instrumentationHook: true,
   },
