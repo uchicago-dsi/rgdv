aadc6167d43248aa4c34ba58eb45fc9cf2f52d96
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-12 14:46:51 -0500

    county page scaffold

--- app/county/[county]/page.tsx
+++ app/county/[county]/page.tsx
@@ -2,7 +2,8 @@ import React from "react"
 import path from "path"
 import { getSummaryStats } from "utils/data/summaryStats"
 import { getContentDirs } from "utils/contentDirs"
-
+import PercentileLineChart from "components/PercentileLineChart"
+import { ArrowLeftIcon } from "@radix-ui/react-icons"
 type CountyRouteProps = {
   params: {
     county: string
@@ -19,22 +20,10 @@ type CountyDataKeys = [
   "hhi_2021_state_percentile",
   "segregation_2021",
   "segregation_2021_percentile",
-  "segregation_2021_state_percentile"
+  "segregation_2021_state_percentile",
 ]
-type CountyDataValues = [
-  string,
-  number,
-  number,
-  number,
-  number,
-  number,
-  number,
-  number,
-  number,
-  number
-]
+type CountyDataValues = [string, number, number, number, number, number, number, number, number, number]
 type CountyDataMap = Map<CountyDataKeys, CountyDataValues>
-
 
 const CountyPage: React.FC<CountyRouteProps> = async ({ params }) => {
   // dynamic routes to use mdx content
@@ -42,15 +31,60 @@ const CountyPage: React.FC<CountyRouteProps> = async ({ params }) => {
   const county = params.county
   const countyDataPath = path.join(process.cwd(), "public", "data", `county_summary_stats.msgpack`)
   const countyStats = await getSummaryStats<CountyDataMap>(countyDataPath, county)
-
+  const data = countyStats.result!
+  const [foodAccess, marketPower, racialEquity] = [
+    // @ts-ignore
+    parseInt(data.get("gravity_2021_percentile")),
+    // @ts-ignore
+    100 - data.get("hhi_2021_percentile"),
+    // @ts-ignore
+    100 - data.get("segregation_2021_percentile"),
+  ]
   return (
-    <div>
-      <h1>{county}</h1>
-      <code>
-      {/* @ts-ignore */}
-
-      {JSON.stringify(Object.fromEntries(countyStats), null, 2)}
-      </code>
+    <div className="bg-canvas-500 min-h-[100vh] p-4">
+      {countyStats.ok === false ? (
+        <div>County not found</div>
+      ) : (
+        <>
+          {/* grid two equal columns
+        collapse on mobile */}
+          <div className="grid gap-8 lg:grid-cols-2">
+            <div>
+              <a href="/county" className="align-center flex items-center pb-2 text-sm text-gray-600">
+                <ArrowLeftIcon className="mr-2 inline h-4 w-4" />
+                Back to counties
+              </a>
+              <div className="prose max-w-none">
+                <h2>COUNTY REPORT</h2>
+                <h1>{county}</h1>
+                <p>
+                  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit
+                  amet Lorem ipsum dolor sit amet{" "}
+                </p>
+              </div>
+            </div>
+            <div>
+              <div className="relative grid gap-8 lg:grid-cols-3">
+                <div className="border-solid border-r-neutral-500 lg:border-r-2 lg:pr-8">
+                  <h3 className="font-weight-900 font-sans text-xl">FOOD ACCESS</h3>
+                  <h4 className="font-serif text-6xl">{foodAccess}</h4>
+                  <PercentileLineChart value={foodAccess} />
+                </div>
+                <div className="border-solid border-r-neutral-500 lg:border-r-2 lg:pr-8">
+                  <h3 className="font-weight-900 font-sans text-xl">MARKET POWER</h3>
+                  <h4 className="font-serif text-6xl">{marketPower}</h4>
+                  <PercentileLineChart value={marketPower} />
+                </div>
+                <div className="lg:mr-8">
+                  <h3 className="font-weight-900 font-sans text-xl">RACIAL EQUITY</h3>
+                  <h4 className="font-serif text-6xl">{racialEquity}</h4>
+                  <PercentileLineChart value={racialEquity} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- app/county/page.tsx
+++ app/county/page.tsx
@@ -1,11 +1,27 @@
-import React from "react"
+"use client"
+import CountyFilterSelector from "components/CountyFilterSelector"
+import { useRouter } from "next/navigation"
+import React, { useState } from "react"
 
-const StatePage: React.FC = () => {
+const CountyPage: React.FC = () => {
+  const [filter, setFilter] = useState<string>("")
+  const router = useRouter()
+  const handleChange = (e: string) => {
+    //`${number}${number}` | `${number}${number}${number}${number}${number}`
+    if (e.length === 5) {
+      router.push(`/county/${e}`)
+    } else {
+      setFilter(e)
+    }
+  }
   return (
-    <div>
-      <h1>State home page</h1>
+    <div className="bg-canvas-500 align-center flex min-h-[100vh] items-center justify-center p-8">
+      <div className="bg-white p-8 shadow-xl">
+        <h1 className="mb-4 text-2xl">County Home Page</h1>
+        <CountyFilterSelector currentFilter={filter} handleSetFilter={handleChange} />
+      </div>
     </div>
   )
 }
 
-export default StatePage
+export default CountyPage
