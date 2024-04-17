88f5b16d3ab3e105c5789ea1da29df0bbc8fafd3
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-17 11:48:28 -0500

    Tooltip component

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -1,48 +1,10 @@
+"use client"
 import * as RadixTooltip from "@radix-ui/react-tooltip"
-import { cva, VariantProps } from "class-variance-authority"
 import React from "react"
 import { twMerge } from "tailwind-merge"
-
-const tooltipContent = cva([], {
-  variants: {
-    intent: {
-      primary: ["rounded-0.5md", "bg-zinc-700", "font-open-sans", "text-white"],
-    },
-    size: {
-      md: ["px-4", "py-2.5", "text-2xs"],
-    },
-  },
-  defaultVariants: {
-    intent: "primary",
-    size: "md",
-  },
-})
-
-const tooltipArrow = cva([], {
-  variants: {
-    intent: {
-      primary: ["fill-zinc-700"],
-    },
-    size: {
-      md: ["w-4", "h-2"],
-    },
-  },
-  defaultVariants: {
-    intent: "primary",
-    size: "md",
-  },
-})
-
-export interface TooltipProps extends VariantProps<typeof tooltipContent>, RadixTooltip.TooltipProps {
-  explainer: React.ReactElement | string
-  children: React.ReactElement
-  className?: string
-  withArrow?: boolean
-  side?: "top" | "right" | "bottom" | "left"
-}
-
+import { tooltipArrow, tooltipContent, TooltipProps } from "./types"
+import { InfoCircledIcon } from "@radix-ui/react-icons"
 export function Tooltip({
-  children,
   explainer,
   open,
   defaultOpen,
@@ -56,7 +18,9 @@ export function Tooltip({
   return (
     <RadixTooltip.Provider>
       <RadixTooltip.Root open={open} defaultOpen={defaultOpen} onOpenChange={onOpenChange} delayDuration={200}>
-        <RadixTooltip.Trigger asChild>{children}</RadixTooltip.Trigger>
+        <RadixTooltip.Trigger asChild className="mx-2">
+          <InfoCircledIcon />
+        </RadixTooltip.Trigger>
         <RadixTooltip.Portal>
           <RadixTooltip.Content
             side={side}
---
+++ components/Tooltip/types.ts
@@ -0,0 +1,49 @@
+import * as RadixTooltip from "@radix-ui/react-tooltip"
+import { cva, VariantProps } from "class-variance-authority"
+
+export const tooltipContent = cva([], {
+  variants: {
+    intent: {
+      primary: [
+        "rounded-0.5md",
+        "bg-white",
+        "shadow-xl", 
+        "border-2",
+        "border-black",
+        "font-serif", 
+        "max-w-[80vw]",
+        "text-black"],
+    },
+    size: {
+      sm: ["px-2.5", "py-1.5", "text-xs"],
+      md: ["px-4", "py-2.5", "text-2xs"],
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+    size: "md",
+  },
+})
+
+export const tooltipArrow = cva([], {
+  variants: {
+    intent: {
+      primary: ["fill-zinc-700"],
+    },
+    size: {
+      sm: ["w-2", "h-1"],
+      md: ["w-4", "h-2"],
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+    size: "md",
+  },
+})
+
+export interface TooltipProps extends VariantProps<typeof tooltipContent>, RadixTooltip.TooltipProps {
+  explainer: React.ReactElement | string
+  className?: string
+  withArrow?: boolean
+  side?: "top" | "right" | "bottom" | "left"
+}
