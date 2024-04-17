4013fbfb3f6af60396a4073fac75c69ab10eb938
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-17 12:09:47 -0500

    home layout updates

--- components/Pages/Home/Renderer.tsx
+++ components/Pages/Home/Renderer.tsx
@@ -5,8 +5,7 @@ export const Renderer: React.FC<HomeProps> = () => {
   // const sections = pageInfo.data.page.sections || []
   return (
     <main className="flex flex-col">
-      <div className="text-theme-canvas-100 relative h-[100vh] w-full" id="main-hero">
-        <div className="">
+      <div className="relative flex flex-col w-full text-theme-canvas-100 min-h-[80vh] justify-end" id="main-hero">
           <Image
             src="/images/hero.png"
             alt="Picture of the author"
@@ -16,35 +15,40 @@ export const Renderer: React.FC<HomeProps> = () => {
               objectPosition: "top middle",
             }}
           />
-        </div>
 
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-theme-navy-500 to-theme-navy-500"></div>
-        <div className="absolute inset-x-0 bottom-0 h-[30vh] bg-theme-navy-500"></div>
-        <div className="text-theme-canvas-500 flex h-full flex-col justify-end p-4">
-          <div className="z-20 grid grid-flow-col grid-rows-2">
-            <div>
-              <h1 className="text-9xl font-bold" style={{WebkitTextStroke: "2px #00152c"}}>Feeding <span className="text-theme-navy-500 block" style={{WebkitTextStroke: "2px white"}}>Fairness</span></h1>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-theme-navy-500 to-theme-navy-500"></div>
+          <div className="absolute inset-x-0 bottom-0 h-[30vh] bg-theme-navy-500"></div>
+          <div className="flex h-full flex-col justify-end p-4 text-theme-canvas-500">
+            <div className="z-20 grid grid-flow-col grid-rows-2 lg:mb-12 lg:grid-cols-2 lg:grid-rows-1">
+              <div>
+                <h1 className="text-[13vw] font-bold leading-[13vw]" style={{ WebkitTextStroke: "2px #00152c" }}>
+                  Feeding{" "}
+                  <span className="block text-theme-navy-500" style={{ WebkitTextStroke: "2px white" }}>
+                    Fairness
+                  </span>
+                </h1>
+              </div>
+              <div className="my-12 lg:my-0">
+                <h2 className="text-3xl font-bold">
+                  Understanding the impacts of structural racism &amp; unfair markets on food access
+                </h2>
+              </div>
             </div>
-            <div>
-              <h2 className="text-3xl font-bold">
-                Understanding the impacts of structural racism &amp; unfair markets on food access
-              </h2>
+            <div className="z-20 grid grid-flow-col grid-rows-2 gap-8 lg:grid-cols-2 lg:grid-rows-1">
+              <div>
+                This tool enables advocates, analytics, and decision-makers to unravel the landscape of inequitable
+                access to food across the US. Use this tool to find opportunities to improve access, break down
+                potential relationships of factors driving inequity, and utilize our data for further analysis and
+                research.
+              </div>
+              <div>
+                Know where you want to see? Search for a state, county, or address below to see a report on food access,
+                market concentration, and structural racism:
+              </div>
             </div>
-          </div>
-          <div className="z-20 grid grid-flow-col grid-rows-2">
-            <div>
-              This tool enables advocates, analytics, and decision-makers to unravel the landscape of inequitable access
-              to food across the US. Use this tool to find opportunities to improve access, break down potential
-              relationships of factors driving inequity, and utilize our data for further analysis and research.
-            </div>
-            <div>
-              Know where you want to see? Search for a state, county, or address below to see a report on food access,
-              market concentration, and structural racism:
-            </div>
-          </div>
         </div>
       </div>
-      <div className="text-theme-canvas-500 border-top-1 h-[50vh] bg-[#00152c] p-4">
+      <div className="border-top-1 h-[50vh] bg-[#00152c] p-4 text-theme-canvas-500">
         <hr />
         {/* 4 div flex layout equal widths */}
         {/* reports, trends, toolkit, about */}
