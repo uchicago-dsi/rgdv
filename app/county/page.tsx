ba3db4684c8646bd24fb867a8c437dc7d455dc94
nofurtherinformation <dhalpern@uchicago.edu>
2024-04-18 09:54:55 -0500

    county page cleanup

--- app/county/page.tsx
+++ app/county/page.tsx
@@ -9,6 +9,7 @@ const CountyPage: React.FC = () => {
   const handleChange = (e: string) => {
     //`${number}${number}` | `${number}${number}${number}${number}${number}`
     if (e.length === 5) {
+      setFilter(e)
       router.push(`/county/${e}`)
     } else if (e.length === 2) {
       setFilter(e)
@@ -17,9 +18,15 @@ const CountyPage: React.FC = () => {
 
   return (
     <div className="bg-canvas-500 align-center flex min-h-[100vh] items-center justify-center p-8">
-      <div className="bg-white p-8 shadow-xl">
-        <h1 className="mb-4 text-2xl">County Home Page</h1>
-        <CountyFilterSelector currentFilter={filter} handleSetFilter={handleChange} />
+      <div className="bg-white p-8 shadow-xl min-w-[50vw] min-h-[20vh]">
+        {filter.length === 5 ? (
+          <code>Loading, please wait...</code>
+        ) : (
+          <>
+            <h1 className="mb-4 text-2xl">County Home Page</h1>
+            <CountyFilterSelector currentFilter={filter} handleSetFilter={handleChange} />
+          </>
+        )}
       </div>
     </div>
   )
