771a23dd5c3468ea3099d35bb15d7b4175413834
nofurtherinformation <dhalpern@uchicago.edu>
2024-05-08 15:41:46 -0500

    stores API route

--- app/api/stores/[geoid]/route.ts
+++ app/api/stores/[geoid]/route.ts
@@ -1,6 +1,7 @@
 "use server"
-import { unpack } from 'msgpackr';
-import nodeFetch from "node-fetch";
+import { unpack } from "msgpackr"
+import { StoreData } from "./types"
+import nodeFetch from "node-fetch"
 
 export type ReqParams = {
   params: {
@@ -8,28 +9,26 @@ export type ReqParams = {
   }
 }
 
-const isochorones: Record<string, Record<string, {geometry:any}>> = {}
+const cache: Record<string, StoreData> = {}
 
-const getIsochrone = async (geoid: string) => {
-  const state =  geoid.slice(0, 2)
-  if (isochorones[state] === undefined) {
-    const r = await nodeFetch(`${process.env.DATA_ENDPOINT}isochrones/isochrones${state}.msgpack`)
+const getStores = async (geoid: string) => {
+  const county = geoid.slice(0, 5)
+  if (cache[county] === undefined) {
+    const r = await nodeFetch(`${process.env.DATA_ENDPOINT}stores${county}.msgpack`)
     const buffer = await r.arrayBuffer()
     const dataBuffer = Buffer.from(buffer)
-    let data = unpack(dataBuffer);
-    isochorones[state] = data
+    let data = unpack(dataBuffer)
+    cache[county] = data as StoreData
   }
-  return isochorones[state]![geoid]
+  return cache[county]?.filter((store) => store.GEOID === geoid) || []
 }
 
 export async function GET(_req: Request, reqParams: ReqParams) {
   const geoid = reqParams.params.geoid
-  if (!geoid || geoid === 'null') {
-    return new Response("Not found", { status: 404 });
+  if (!geoid || geoid === "null") {
+    return new Response("Not found", { status: 404 })
   }
-  const data = await getIsochrone(geoid);
-  // console.log(data)
-  // @ts-ignore
+  const data = await getStores(geoid)
   return new Response(JSON.stringify(data), {
     headers: {
       "content-type": "application/json",
---
+++ app/api/stores/[geoid]/types.ts
@@ -0,0 +1,7 @@
+export type StoreData = Array<{
+  GEOID: string
+  LAT: number
+  LON: number
+  COMPANY: string
+  "PARENT COMPANY": string
+}>
