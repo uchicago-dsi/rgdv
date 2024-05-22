b9f06b5e4cbb9ebe9acbd59d075f44b2ee46383c
nofurtherinformation <dhalpern@uchicago.edu>
2024-05-22 11:40:22 -0500

    changelog

---
+++ app/changelog/page.tsx
@@ -0,0 +1,25 @@
+const log = {
+  "5/22": ["Added pie charts to reports", "Added changelog page", "Added other ancillary pages"],
+  "5/21": [
+    "Refactored and centralized report formatting",
+    "Enhanced percentile scale for greater legibility",
+    "Swapped 'score' scales to align with value conceptual meaning (eg. high segregation score is bad)",
+  ],
+} as const
+export default function Page() {
+  return (
+    <div className="prose px-4">
+      {Object.keys(log).map((date) => (
+        <div>
+          <h2>{date}</h2>
+          <ul>
+            {/* @ts-ignore */}
+            {log[date].map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
+}
