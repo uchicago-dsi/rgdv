ce0ca16295e9c3b3732348c080e69924797bdd48
nofurtherinformation <dhalpern@uchicago.edu>
2024-06-07 13:56:03 -0500

    changelog update

--- app/national/page.tsx
+++ app/national/page.tsx
@@ -1,10 +1,14 @@
+
 import React from "react"
 import ReportLayout from "components/ReportLayout"
 import { getContentDirs } from "utils/contentDirs"
 
 const NationalPage = async () => {
   getContentDirs()
-  return <ReportLayout id={'1'} showHeader={false} />
+  return <>
+  <h1 className="prose text-4xl p-8 text-center w-full max-w-none">National Report</h1>
+  <ReportLayout id={'1'} showHeader={false} />
+  </>
 }
 
 export default NationalPage
