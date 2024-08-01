a942ce78cb06dfd4f334d882bc26cc5223fb92db
nofurtherinformation <dhalpern@uchicago.edu>
2024-08-01 14:02:56 -0500

    parse jsx elements

--- utils/mdx/parseRich.ts
+++ utils/mdx/parseRich.ts
@@ -1,7 +1,53 @@
 import { parseMDX } from "@tinacms/mdx"
 
+const cleanHtmlJsx = (elementName: string, htmlChild: any) => {
+  let newElement: any = {
+    type: "mdxJsxFlowElement",
+    name: elementName,
+    children: htmlChild.children,
+    props: {},
+  }
+
+  const propsRegex = /(\w+)=({[^}]+}|".*?")/g
+  let match
+  let props: any = {}
+
+  while ((match = propsRegex.exec(htmlChild.value)) !== null) {
+    const propName = match[1]
+    const propValue = match[2]
+    if (!propName || !propValue) {
+      continue
+    }
+    // Process the prop value
+    if (propValue.startsWith("<>") && propValue.endsWith("</>")) {
+      // For JSX elements, parse the content
+      const parsed = parseRich(propValue.slice(3, -3))
+      props[propName] = parsed
+    } else if (propValue.startsWith("{<>") && propValue.endsWith("</>}")) {
+      // For JSX elements, parse the content
+      const parsed = parseRich(propValue.slice(4, -4))
+      props[propName] = parsed
+    } else if (propValue.startsWith("{") && propValue.endsWith("}")) {
+      // For values enclosed in {}, remove the braces
+      props[propName] = propValue.slice(1, -1).trim()
+    } else if (propValue.startsWith('"') && propValue.endsWith('"')) {
+      // For values enclosed in quotes, remove the quotes
+      props[propName] = propValue.slice(1, -1)
+    } else if (propValue.startsWith("<>") && propValue.endsWith("</>")) {
+      // For JSX elements, parse the content
+      const parsed = parseRich(propValue.slice(3, -3))
+      props[propName] = parsed
+    } else {
+      // For other cases, use the raw value
+      props[propName] = propValue
+    }
+  }
+  newElement.props = props
+  return newElement
+}
+
 const parseRich = (mdxContent: string) => {
-  return parseMDX(
+  let c = parseMDX(
     mdxContent,
     {
       name: "body", // This is the key in your data for the rich text content
@@ -12,6 +58,26 @@ const parseRich = (mdxContent: string) => {
     },
     (f: any) => f
   )
+  if (c.children.some((f) => f.type === "html")) {
+    const htmlChildren = []
+    const htmlChildrenIndexes = []
+
+    for (let i = 0; i < c.children.length; i++) {
+      if (c?.children?.[i]?.type === "html") {
+        htmlChildren.push(c.children[i])
+        htmlChildrenIndexes.push(i)
+      }
+    }
+
+    for (let i = 0; i < htmlChildren.length; i++) {
+      const htmlChild = htmlChildren[i] as any
+      const htmlChildIndex = htmlChildrenIndexes[i]!
+      const elementName = htmlChild.value.split("\n")[0].split(" ")[0].replace("<", "")
+      const newElement = cleanHtmlJsx(elementName, htmlChild)
+      c.children[htmlChildIndex] = newElement
+    }
+  }
+  return c
 }
 
 export default parseRich
