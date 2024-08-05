174acf67acebd4f6fee5c3f3dc1d1f0b4230a783
nofurtherinformation <dhalpern@uchicago.edu>
2024-08-05 11:11:03 -0500

    Select cleanup

--- components/CountyFilterSelector/CountyFilterSelector.tsx
+++ components/CountyFilterSelector/CountyFilterSelector.tsx
@@ -5,6 +5,20 @@ import { SelectMenu } from "components/Select/Select"
 import CountyList from "./county_list.json"
 import type { CountyFilterSelectorProps } from "./types"
 
+const StyledSelectItem: React.FC<{ children: React.ReactNode; value: any, disabled?: boolean }> = ({ children, value, disabled=false }) => {
+  return (
+    <Select.Item
+    disabled={disabled}
+      className={`
+        ${disabled ? "text-bold text-black" : "text-violet11"}
+        SelectItem relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none text-violet11 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1 data-[highlighted]:outline-none`}
+      value={value as string}
+    >
+      {children}
+    </Select.Item>
+  )
+}
+
 export const CountyFilterSelector: React.FC<CountyFilterSelectorProps> = ({ handleSetFilter, currentFilter, size }) => {
   const _fontSize = size
   const filterState = currentFilter?.length ? currentFilter.slice(0, 2) : ""
@@ -17,49 +31,45 @@ export const CountyFilterSelector: React.FC<CountyFilterSelectorProps> = ({ hand
     <div>
       <SelectMenu title="Filter by state" value={currentState} onValueChange={handleSetFilter}>
         <>
-          <Select.Item className="SelectItem" value={currentState}>
+          <StyledSelectItem value={currentState} disabled>
             <Select.ItemText>{currentState || "Choose a State"}</Select.ItemText>
             <Select.ItemIndicator className="SelectItemIndicator">
               <CheckboxIcon />
             </Select.ItemIndicator>
-          </Select.Item>
+          </StyledSelectItem>
           {/* @ts-ignore */}
-          <Select.Item className="SelectItem" value={null}>
+          <StyledSelectItem value={null}>
             <Select.ItemText>All</Select.ItemText>
             <Select.ItemIndicator className="SelectItemIndicator">
               <CheckboxIcon />
             </Select.ItemIndicator>
-          </Select.Item>
+          </StyledSelectItem>
           {CountyList.map((state, i) => (
-            <Select.Item
-              className="relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none text-violet11 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1 data-[highlighted]:outline-none"
-              value={state.statefp as string}
-              key={i}
-            >
+            <StyledSelectItem value={state.statefp} key={i}>
               <Select.ItemText>{state.state}</Select.ItemText>
               <Select.ItemIndicator className="SelectItemIndicator">
                 <CheckboxIcon />
               </Select.ItemIndicator>
-            </Select.Item>
+            </StyledSelectItem>
           ))}
         </>
       </SelectMenu>
       {currentCounties.length > 0 && (
         <SelectMenu title="Filter by county" value={currentCounty} onValueChange={handleSetFilter}>
           <>
-            <Select.Item className="SelectItem" value={currentCounty}>
+            <StyledSelectItem value={currentCounty}>
               <Select.ItemText>{currentCounty || "Choose a County"}</Select.ItemText>
               <Select.ItemIndicator className="SelectItemIndicator">
                 <CheckboxIcon />
               </Select.ItemIndicator>
-            </Select.Item>
+            </StyledSelectItem>
             {currentCounties.map((county: any, i: number) => (
-              <Select.Item className="SelectItem" value={county.fips} key={i}>
+              <StyledSelectItem value={county.fips} key={i}>
                 <Select.ItemText>{county.name}</Select.ItemText>
                 <Select.ItemIndicator className="SelectItemIndicator">
                   <CheckboxIcon />
                 </Select.ItemIndicator>
-              </Select.Item>
+              </StyledSelectItem>
             ))}
           </>
         </SelectMenu>
